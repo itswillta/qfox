82d567b1180f805b3867f2259db512f154b61949
Huyen Phi Khanh <phihuyen97@gmail.com>
2019-03-29 18:29:04 +0700

    Issue #15: Added enum and fixed response

---
+++ backend/app/Enums/ClassRole.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class ClassRole extends Enum
+{
+    const OWNER = "Owner";
+    const MEMBER = "Member";
+}
---
+++ backend/app/Helpers/ResponseFormatter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Helpers;
+
+
+use Illuminate\Validation\Validator;
+
+class ResponseFormatter
+{
+    public static function flattenValidatorErrors(Validator $validator)
+    {
+        $details = [];
+        foreach ($validator->errors()->toArray() as $field => $value) {
+            $details[$field] = $value[0];
+        }
+
+        return $details;
+    }
+}
