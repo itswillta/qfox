3be6bd7e93154ed5f96f714f41ca04918c51a349
Huy Ta Quoc <quochuy.tl.bk@gmail.com>
2019-04-06 22:04:47 +0700

    Issue #53: Fixed wrongly-applied middlewares bug that breaks POST API endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\ValidateUserPermission;
 use App\Http\Middleware\StudyClass\ValidateClassEditPermission;
 use App\Http\Middleware\StudySet\ValidateSetEditPermission;
 use App\Http\Middleware\Term\ValidateTermEditPermission;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -28,25 +29,31 @@ Route::group(['middleware' => ['api', 'jwt.auth']], function () {
         Route::put('/{user_id}', 'UserController@update');
     });
 
-    Route::group(['middleware' => ValidateClassEditPermission::class, 'prefix' => 'users/{user_id}/classes'], function () {
+    Route::group(['middleware' => ValidateUserPermission::class, 'prefix' => 'users/{user_id}/classes'], function () {
         Route::post('', 'ClassController@create');
-        Route::put('/{class_id}', 'ClassController@update');
-        Route::post('/{class_id}/study-sets', 'ClassController@addStudySet');
-        Route::delete('/{class_id}', 'ClassController@delete');
-        Route::post('/{class_id}/members', 'ClassController@addMember');
-        Route::delete('/{class_id}/members', 'ClassController@removeMembers');
-        Route::delete('/{class_id}/study-sets', 'ClassController@removeStudySets');
+        Route::group(['middleware' => ValidateClassEditPermission::class], function () {
+            Route::put('/{class_id}', 'ClassController@update');
+            Route::post('/{class_id}/study-sets', 'ClassController@addStudySet');
+            Route::delete('/{class_id}', 'ClassController@delete');
+            Route::post('/{class_id}/members', 'ClassController@addMember');
+            Route::delete('/{class_id}/members', 'ClassController@removeMembers');
+            Route::delete('/{class_id}/study-sets', 'ClassController@removeStudySets');
+        });
     });
 
-    Route::group(['middleware' => ValidateSetEditPermission::class, 'prefix' => 'users/{user_id}/study-sets'], function () {
+    Route::group(['middleware' => ValidateUserPermission::class, 'prefix' => 'users/{user_id}/study-sets'], function () {
         Route::post('', 'StudySetController@create');
-        Route::put('/{study_set_id}', 'StudySetController@update');
-        Route::delete('/{study_set_id}', 'StudySetController@delete');
+        Route::group(['middleware' => ValidateSetEditPermission::class], function () {
+            Route::put('/{study_set_id}', 'StudySetController@update');
+            Route::delete('/{study_set_id}', 'StudySetController@delete');
+        });
     });
 
-    Route::group(['middleware' => [ValidateSetEditPermission::class, ValidateTermEditPermission::class], 'prefix' => 'users/{user_id}/study-sets/{study_set_id}/terms'], function () {
+    Route::group(['middleware' => [ValidateSetEditPermission::class], 'prefix' => 'users/{user_id}/study-sets/{study_set_id}/terms'], function () {
         Route::post('', 'TermController@create');
-        Route::put('/{term_id}', 'TermController@update');
-        Route::delete('/{term_id}', 'TermController@delete');
+        Route::group(['middleware' => ValidateTermEditPermission::class], function () {
+            Route::put('/{term_id}', 'TermController@update');
+            Route::delete('/{term_id}', 'TermController@delete');
+        });
     });
 });
