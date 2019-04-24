0d667e97e702a733aa26d924918e59760c68e85a
Huy Ta Quoc <quochuy.tl.bk@gmail.com>
2019-04-24 11:51:57 +0700

    Issue #66: Added script to re-install a newer version of the whole app

--- cmd/installation-linux.sh
+++ cmd/installation-linux.sh
@@ -6,11 +6,14 @@
 
 #WORKDIR: Project Root
 
+docker-compose down
+
 cd backend
 
 #WORKDIR: backend
 
-[ ! -d "./vendor" ] && docker container run --rm -v $(pwd):/app composer install
+sudo rm -rf ./vendor
+docker container run --rm -v $(pwd):/app composer install
 sudo chown -R $USER:$USER .
 cp .env.example .env
 
@@ -18,30 +21,29 @@ cd ../frontend
 
 #WORKDIR: frontend
 
-[ ! -d "./node_modules" ] && docker container run --rm -v $(pwd):/app node:11-alpine sh -c "cd /app && npm install"
+sudo rm -rf ./node_modules
+sudo rm -rf ./dist
+sudo rm -rf ./.cache
+docker container run --rm -v $(pwd):/app node:11-alpine sh -c "cd /app && npm install"
 
 cd ..
 
 #WORKDIR: Project Root
 
 docker volume rm qfox_dbdata
+docker volume rm qfox_els-data
 
-docker-compose up --build -d
+docker-compose build --force-rm --no-cache
+docker-compose up -d
 
-docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan cache:clear
+docker-compose exec app php artisan route:clear
 docker-compose exec app php artisan config:clear
-docker-compose exec app php artisan config:cache
 docker-compose exec app php artisan optimize
-
-cd backend
-
-#WORKDIR: backend
-
-docker container run --rm -v $(pwd):/app composer dump-autoload
+docker-compose exec app composer dump-autoload
 docker-compose exec app php artisan migrate:refresh --seed
-
-cd ..
-
-#WORKDIR: Project Root
+docker-compose exec app php artisan jwt:secret
+docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan config:cache
 
 docker-compose down
