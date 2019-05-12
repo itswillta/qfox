14e93a8c97805544f01480724c9fec13fd08b6a8
baobi <mgbanh97@gmail.com>
2019-05-12 20:38:35 +0700

    Issue #75: Added login with google

--- backend/config/services.php
+++ backend/config/services.php
@@ -48,5 +48,10 @@ return [
         'client_secret' => env('FACEBOOK_APP_SECRET'),
         'redirect' => env('FACEBOOK_APP_CALLBACK_URL'),
     ],
+    'google' => [
+        'client_id' => env('GOOGLE_APP_ID'),
+        'client_secret' => env('GOOGLE_APP_SECRET'),
+        'redirect' => env('GOOGLE_APP_CALLBACK_URL'),
+    ],
 
 ];
